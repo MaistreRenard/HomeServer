76c33de0a2c5d84a92a170dffb8105211c7b2e0a
Nicolas Wirth <116091834+MaistreRenard@users.noreply.github.com>
2025-10-12 15:02:58 +0200

    main: Fix formatting issues
    Signed-off-by: Nicolas Wirth <116091834+MaistreRenard@users.noreply.github.com>

--- configuration.nix
+++ configuration.nix
@@ -1,41 +1,41 @@
 { config, modulesPath, pkgs, lib, ... }:
 let
-  # DO NOT COMMIT
-  secrets = import ./private/secrets.nix;
+	# DO NOT COMMIT
+	secrets = import ./private/secrets.nix;
 in
-{
-  imports = [
-	./modules/proxmox-lxc.nix
-	./modules/openssh.nix
-	./modules/utils.nix
-	./modules/neovim.nix
-	./modules/tailscale.nix
-  ];
+	{
+	imports = [
+		./modules/proxmox-lxc.nix
+		./modules/openssh.nix
+		./modules/utils.nix
+		./modules/neovim.nix
+		./modules/tailscale.nix
+	];
 
-  # Base user
-  users.users.nicoco = {
-	createHome = true;
-	isNormalUser  = true;
-	extraGroups = [ "wheel" ];
-  home = "/home/nicoco";
-  };
+	# Base user
+	users.users.nicoco = {
+		createHome = true;
+		isNormalUser  = true;
+		extraGroups = [ "wheel" ];
+		home = "/home/nicoco";
+	};
 
- # # To mount NFS share
- #  boot.supportedFilesystems = [ "nfs" ];
- #
- #  # Media Server
- #  fileSystems."/mnt/TrueNas-Media" =
- #    {
- #      device = "${secrets.nasHost}:${secrets.nasMedia}";
- #      fsType = "nfs4";
- #    };
- #
- #  # Configuration
- #  fileSystems."/mnt/TrueNas-Configuration" =
- #    {
- #      device = "${secrets.nasHost}:${secrets.nasConf}";
- #      fsType = "nfs4";
- #    };
- #
-  system.stateVersion = "25.05";
+	# # To mount NFS share
+	#  boot.supportedFilesystems = [ "nfs" ];
+	#
+	#  # Media Server
+	#  fileSystems."/mnt/TrueNas-Media" =
+	#    {
+	#      device = "${secrets.nasHost}:${secrets.nasMedia}";
+	#      fsType = "nfs4";
+	#    };
+	#
+	#  # Configuration
+	#  fileSystems."/mnt/TrueNas-Configuration" =
+	#    {
+	#      device = "${secrets.nasHost}:${secrets.nasConf}";
+	#      fsType = "nfs4";
+	#    };
+	#
+	system.stateVersion = "25.05";
 }
