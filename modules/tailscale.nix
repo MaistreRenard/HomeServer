7e5af42e3acd09876a00408cf1fbc1f4b49ee43d
Nicolas Wirth <116091834+MaistreRenard@users.noreply.github.com>
2026-03-09 15:44:59 +0000

    CT-NixOS-Tailscale: Initial Configuration
    Signed-off-by: Nicolas Wirth <116091834+MaistreRenard@users.noreply.github.com>

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -1,6 +1,33 @@
 { pkgs, ... }:
+let
+  secrets = import ../private/secrets.nix;
+in
 {
   services.tailscale = {
     enable = true;
+    useRoutingFeatures = "server";
   };
+
+  systemd.services.tailscale-up = {
+    description = "Tailscale up";
+    after = [ "tailscaled.service" ];
+    wants = [ "tailscaled.service" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = ''
+        ${pkgs.tailscale}/bin/tailscale up \
+          --advertise-routes=${secrets.tailscaleHome} \
+          --accept-routes \
+          --snat-subnet-routes=false \
+          --ssh \
+          --reset
+      '';
+    };
+  };
+
+  # boot.kernel.sysctl = {
+  #   "net.ipv4.ip_forward" = 1;
+  #   "net.ipv6.conf.all.forwarding" = 1;
+  # };
 }
